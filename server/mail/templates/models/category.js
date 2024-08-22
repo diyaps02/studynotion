cb2ee5eccf91eda476c56262f8df26d0350a9c0d
diyaps02 <140656509+diyaps02@users.noreply.github.com>
2024-08-22 16:15:20 +0530

    Delete server/mail/templates/models directory

--- server/mail/templates/models/category.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require("mongoose");
-const categoryschema = new mongoose.Schema({
-  name: { type: String, required: true },
-  description: { type: String, trim: true },
-  courses: [{ type: mongoose.Types.ObjectId, ref: "Course" }],
-});
-module.exports = mongoose.model("Category", categoryschema);
--- server/mail/templates/models/subsection.js
+++
@@ -1,8 +0,0 @@
-const mongoose=require("mongoose");
-const subsection = new mongoose.Schema({
-title:{type:String},
-timeduration:{type:String},
-description:{type:String},
-videourl:{type:String}
-});
-module.exports=mongoose.model("Subsection",subsection);
