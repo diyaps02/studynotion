e44cd70487c27a30387243971a015df3f36437a1
diyaps02 <140656509+diyaps02@users.noreply.github.com>
2024-08-22 22:28:16 +0530

    Update database.js

--- server/config/database.js
+++ server/config/database.js
@@ -3,8 +3,8 @@ require("dotenv").config();
 
 const database= async(req,res)=>{
  mongoose.connect(process.env.DATABASE_URL,
-    {useNewUrlParser:true},
-    {useUnifiedTopology: true}
+    {useNewUrlParser:true,
+    useUnifiedTopology: true}
     )
     .then(()=>{console.log( "Database Connected Successfully!" )})
     .catch((err) => {console.log(`Error Connecting to Database ${err}`)
